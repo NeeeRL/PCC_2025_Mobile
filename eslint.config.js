56c27bfffe2bba252eeec697e4d07198de94c64c
NeeeRL <kenken.s5049@gmail.com>
2025-05-15 07:20:37 +0900

    プラグインを導入した

--- eslint.config.js
+++ eslint.config.js
@@ -3,11 +3,14 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import reactX  from 'eslint-plugin-react-x'
+import reactDom from 'eslint-plugin-react-dom'
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    // 厳格な型チェックを行う
+    extends: [js.configs.recommended, ...tseslint.configs.strictTypeChecked, ...tseslint.configs.stylisticTypeChecked],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -15,6 +18,8 @@ export default tseslint.config(
     },
     plugins: {
       'react-hooks': reactHooks,
+      'react-x': reactX,
+      'react-dom': reactDom,
       'react-refresh': reactRefresh,
     },
     rules: {
